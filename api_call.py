78698dfc5bad0572de8ff27fe5bc35a97756b11c
EhabMohamedsaid <forehab@hotmail.com>
2021-02-11 20:35:19 +0000

    Corrected method call for os.path

--- api_call.py
+++ api_call.py
@@ -10,7 +10,7 @@ def api_endpoint():
     return "https://www.londonprayertimes.com/api/times/?format=json&key=70bb0d1d-7d54-4d3e-868e-264ac8b88f91&24hours=true&year=" + year
 
 def timetable_exists():
-    return os.path.isfile("./yearlyCalendarOutput/Calendar" + year + ".json")
+    return os.path.exists("./yearlyCalendarOutput/Calendar" + year + ".json")
 
 def download_json(api_endpoint):
     json_file = requests.get(api_endpoint)
