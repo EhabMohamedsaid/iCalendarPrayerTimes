252997d4f9a65df25ff83a971ef328d189b3c976
EhabMohamedsaid <forehab@hotmail.com>
2021-03-28 07:13:36 +0100

    Added dst calculator

--- json_parser.py
+++ json_parser.py
@@ -4,33 +4,40 @@ import ics
 import pytz
 
 year = str(datetime.date.today().year)
-timeZone = pytz.timezone("Europe/London")
+
 
 def read_json():
     with open("./yearlyCalendarOutput/Calendar" + year +".json") as file:
         data = json.load(file)
     generate_calendar(data)
 
+
 def is_dst(dt,timeZone):
    aware_dt = timeZone.localize(dt)
    return aware_dt.dst() != datetime.timedelta(0,0)
+
+
+timeZone = pytz.timezone("Europe/London")
+
 
 def generate_calendar(data):
     no_alarm = ics.Calendar()
 
     for key, value in dict.items(data['times']):
         date = key
-        dt = datetime.datetime(date)
+        dt = datetime.datetime.fromisoformat(date)
         for k, v in dict.items(data['times'][key]):
             if not k == "date":
                 if not k == "asr_2":
                     silent_event = ics.Event()
+                    prayer_time = date + ' ' + v + ':00'
 
                     # subtracts 1 hour from dst due to overcorrection on calendars
                     if is_dst(dt,timeZone):
-                        date = date - datetime.timedelta(hours=1)
-
-                    prayer_time = date + ' ' + v + ':00'
+                        time = datetime.datetime.strptime(v, '%H:%M')
+                        adjusted_time = time - datetime.timedelta(hours=1)
+                        timestamp = adjusted_time.time()
+                        prayer_time = date + ' ' + timestamp.strftime('%H:%M') + ':00'
 
                     # Replace underscores with spaces
                     name = k.replace("_", " ")
