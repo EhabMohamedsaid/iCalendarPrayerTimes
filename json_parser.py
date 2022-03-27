18e61a54e586f5116403b3bad600aea8bd9c10cd
EhabMohamedsaid <forehab@hotmail.com>
2022-03-27 17:09:45 +0100

    Fixed DST overcorrection

--- json_parser.py
+++ json_parser.py
@@ -1,6 +1,7 @@
 import datetime
 import json
 import ics
+import pytz
 
 year = str(datetime.date.today().year)
 
@@ -10,12 +11,18 @@ def read_json():
         data = json.load(file)
     generate_calendar(data)
 
+def is_dst(dt,timeZone):
+   aware_dt = timeZone.localize(dt)
+   return aware_dt.dst() != datetime.timedelta(0)
+
+timeZone = pytz.timezone("Europe/London")
 
 def generate_calendar(data):
     calendar = ics.Calendar()
 
     for key, value in dict.items(data['times']):
         date = key
+        dt = datetime.datetime.fromisoformat(date)
         for k, v in dict.items(data['times'][key]):
             if not k == "date":
                 if not k == "asr_2":
@@ -24,6 +31,13 @@ def generate_calendar(data):
                     prayer_time = date + ' ' + v + ':00'
 
                     silent_events = ["jamat", "sunrise"]
+
+                    #subtracts 1 hour from dst due to overcorrection on smartphone calendars
+                    if is_dst(dt,timeZone):
+                         time = datetime.datetime.strptime(v, '%H:%M')
+                         adjusted_time = time - datetime.timedelta(hours=1)
+                         timestamp = adjusted_time.time()
+                         prayer_time = date + ' ' + timestamp.strftime('%H:%M') + ':00'
 
                     # Replace underscores with spaces
                     name = k.replace("_", " ")
